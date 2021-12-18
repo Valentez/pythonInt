544260411c2a662a18f2e4d61367b5fd965357af
Alexey Dunin <valentez.alkalinella@gmail.com>
2021-12-18 10:40:24 +0300

    Complete

---
+++ hw1.py
@@ -0,0 +1,11 @@
+"""
+1. Поработайте с переменными, создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+"""
+
+a = "wolf"
+b = "walkers"
+print(f'Lets talk about {a}{b} ')
+r = int(input("Yours rating is(only numbers)? "))
+o = input("Yours opinion is? ")
+print(f"Fairy tail about {a}s for you is {r}/10 amd {o}")
