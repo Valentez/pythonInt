700733d3868bf67917edbffd9800f214c3e201eb
Valentez <61311401+Valentez@users.noreply.github.com>
2023-03-01 14:35:05 +0300

    Update hw55.py

--- hw55.py
+++ hw55.py
@@ -3,12 +3,12 @@
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
 
-with open("hw55.txt", "w") as fileWrite:
-    inputNum = input("please, input numbers via space: ")
-    print(inputNum, file=fileWrite)
+with open("hw55.txt", "w") as file_write:
+    input_num = input("please, input numbers via space: ")
+    print(input_num, file=file_write)
 
-with open("hw55.txt") as fileRead:
-    content = fileRead.read().rstrip().split()
-    listNum = [int(num) for num in content if num.isdigit()]
-    print(listNum)
-    print(sum(listNum))
+with open("hw55.txt") as file_read:
+    content = file_read.read().rstrip().split()
+    list_num = [int(num) for num in content if num.isdigit()]
+    print(list_num)
+    print(sum(list_num))
