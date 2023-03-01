e19945cee843a90de8e9e75c241eb01163d66e0b
Valentez <61311401+Valentez@users.noreply.github.com>
2023-03-01 14:46:42 +0300

    Update hw63.py

--- hw63.py
+++ hw63.py
@@ -1,16 +1,16 @@
 """
-Реализовать базовый класс Worker (работник), в котором определить атрибуты:
-name, surname, position (должность), income (доход).
-Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
-оклад и премия, например, {"wage": wage, "bonus": bonus}.
-Создать класс Position (должность) на базе класса Worker.
-В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом
-премии (get_total_income). Проверить работу примера на реальных данных
-(создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы
-экземпляров).
+3.  Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+        name, surname, position (должность), income (доход).
+    Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+        оклад и премия, например, {"wage": wage, "bonus": bonus}.
+    Создать класс Position (должность) на базе класса Worker.
+    В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода
+    с учетом премии (get_total_income). Проверить работу примера на реальных данных
+    (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
 """
 
 class Worker:
+
     def __init__(self, name, surname, position, wage, bonus):
         self.name = name
         self.surname = surname
@@ -20,12 +20,12 @@ class Worker:
 
 class Position(Worker):
 
-    def getFullName(self):
+    def get_full_name(self):
         return f"{self.name} {self.surname}"
 
-    def getFullSalary(self):
+    def get_full_salary(self):
         return self._income['wage'] + self._income['bonus']
 
 
-myPosition = Position('Alex', 'Smith', 'Mage', 110000, 30000)
-print(f'Total salary of {myPosition.getFullName()} is {myPosition.getFullSalary()}')
+my_position = Position('Ivan', 'Ivanov', 'programmer', 110000, 30000)
+print(f'Total salary of {my_position.get_full_name()} is {my_position.get_full_salary()}')
