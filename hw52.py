6aebda06a8c13e9d9d8815ce4e4c4453ce8e8212
Valentez <61311401+Valentez@users.noreply.github.com>
2023-03-01 14:29:10 +0300

    Update hw52.py

--- hw52.py
+++ hw52.py
@@ -3,7 +3,7 @@
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
 with open("hw52.txt") as file:
-    fileLines = file.readlines()
-    print("Value of strings in file: ", len(fileLines))
-    for lineNumber, l in enumerate(fileLines, 1):
-        print(f"Value of words in string {lineNumber}:", len(l.split()))
+    file_lines = file.readlines()
+    print("Value of strings in file: ", len(file_lines))
+    for line_number, l in enumerate(file_lines, 1):
+        print(f"Value of words in string {line_number}:", len(l.split()))
